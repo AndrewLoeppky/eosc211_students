4e9ba0648aef7fc2326b61c3b2b2c6c8f6d2b06c
Phil Austin <paustin@eos.ubc.ca>
2020-06-16 13:08:32 -0700

    make programs installable

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# Setup script for PyPI; use CMakeFile.txt to build extension modules
+
+from setuptools import setup
+
+
+setup(
+    name='eosc211',
+    packages=['e211_lib'],
+    classifiers=[
+        'License :: OSI Approved :: BSD License'
+    ],
+    entry_points={
+          'console_scripts': [
+              'image_extract = e211_lib.image_extract:main',
+              'build_notebook = e211_lib.build_notebook:main'
+          ]
+    },
+    install_requires=['pillow','python-pptx','jupytext'],
+    keywords='image powerpoint',
+    long_description="""extract powerpoint images""")
