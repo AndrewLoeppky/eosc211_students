125132bb62d44fdf835f3d7ac3f709c027385b4e
Andrew Loeppky <Andrew.Loeppky@Gmail.com>
2021-06-11 10:48:22 -0600

    create function library for lab3

---
+++ e211_python_2021/wk03/lab_wk3/e211.py
@@ -0,0 +1,32 @@
+# %%
+'''
+e211 Function library
+
+author: Andrew Loeppky
+course: eosc 211 - computer methods for earth, ocean and atmospheric scientists
+''';
+
+# %%
+from PIL import Image
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+
+# %%
+def load_oceancolor(my_image):
+    """
+    takes in a png image and returns a scaled numpy array. Scaling is set for
+    https://oceancolor.gsfc.nasa.gov chlorophyl concentration files
+    """
+    img_in = Image.open(my_image)
+    img_np = np.asarray(img_in)
+    # default img -- 255->0mg/m3, 0->20mg/m3
+    img_scaled = -(img_np - 255) * (20 / 256) # this should acrualbe log scaled
+    return img_scaled
+
+
+chlor = load_oceancolor("chlorophyl_march1999.png")
+np.max(chlor)
+
+# %%
