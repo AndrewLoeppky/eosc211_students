8b57235af65c03ef6a7cbe315328c5563d523bd3
Andrew Loeppky <Andrew.Loeppky@Gmail.com>
2021-06-16 17:15:16 -0600

    checkpoint3 lab 4

--- e211_python_2021/wk04/lab_wk4/lab_wk4.py
+++ e211_python_2021/wk04/lab_wk4/lab_wk4.py
@@ -47,12 +47,19 @@ from e211_lib import e211
 import numpy as np
 from matplotlib import pyplot as plt 
 
+# %%
+
 # %% [markdown]
 # What does this do?
 
 # %%
 z = 2
 x = round(np.exp(np.sqrt(z / 3)))
+
+# %% [markdown]
+# ### Demo: S
+
+# %%
 
 # %% [markdown]
 # ### Clausius-Clapeyron equation and the atmospheric equation of state
@@ -79,7 +86,7 @@ x = round(np.exp(np.sqrt(z / 3)))
 # e_s = e_0\cdot exp \left[ \frac{L}{R_v}\frac{1}{T_0}-\frac{1}{T}\right]\tag{Clausius-Clapeyron Eqn}
 # $$
 #
-# where $e_0 = 611Pa$, $L= 2.5\cdot10^6J/kg$ is the latent heat of evaporation, $T_0= 273K$ is a constant (NOT $273.15K$), and $R_v= 461J/K/kg (Joules per kelvin per kilogram) is the gas constant for pure water vapour. Next we have
+# where $e_0 = 611Pa$, $L= 2.5\cdot10^6J/kg$ is the latent heat of evaporation, $T_0= 273K$ is a constant (NOT $273.15K$), and $R_v= 461J/K/kg$ (Joules per kelvin per kilogram) is the gas constant for pure water vapour. Next we have
 #
 # $$
 # e = e_s\frac{RH}{100}
@@ -106,5 +113,26 @@ x = round(np.exp(np.sqrt(z / 3)))
 # † See *Meteorology Today for Scientists and Engineers*, R. Stull
 #
 # ‡ You may recognize some similarity between this last equation and the IDEAL GAS LAW $P V=nRT$ which is no coincidence - in fact the $0.61r$ is a correction for the non-ideal nature of moist air.
+
+# %% [markdown]
+# ### Implementing the CC Equation in Python
+#
+# #### Four important points about programming style:
+#
+# * Be very careful with unit conversion.  Humans usually like to express temperature in Celsius, but the thermodynamic equations require temperatures in Kelvin.  That means that you need to distinguish between two temperatures with names like `Tk` and `Tc`, where `Tk = Tc + 273.15`. Make sure that you convert to Kelvin as soon as you can. Do it only once at the start and (if necessary for output) once at the end.
+#
+# * Choose good variable names. Don’t use single letters like `T`, `e` and `P` for variable names in a script. It makes it very hard to do search and replace with your editor when you want to change them, and the names aren’t very meaningful to others who might read your code. Also remember that Python is *case sensitive* so `P` and `p` are two different variables.
+#
+# * Just like the week 3 lab, we will endeavor to keep our code organized and readable. As much as you can, separate your script into 4 parts IN THIS ORDER:
+#
+# **1.  INPUTS:**  These  are  variables  whose  values  you  might  want  to  change  if  you  run  the  programmultiple times.
+#
+# **2.  INTERNAL DEFINITIONS or CONSTANTS:** These are variables that are defined as constantswhich you will never change, or some simple conversions (e.g. Celcius to Kelvin)
+#
+# **3.  CALCULATIONS:** This is where the complicated parts of the procedure go
+#
+# **4.  the OUTPUTS:** What you do once the math is finished (e.g. plotting).
+#
+# Also, It is useful to separate the parts by white space, and liberally comment your code. Note that not all tasks in 2 and 3 are easily separated, and sometimes plotting happens earlier than the end of the program, but it’s a good idea to always approach problems this way initially.
 
 # %%
